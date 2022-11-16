db0d165bb084ae32ece8b013011eacbc7d7138f9
yair mathez <yair_mathez_9@hotmail.com>
2022-11-15 21:29:21 -0300

    Seguimos Modificando

--- TiendaCelulares/src/Menu.java
+++ TiendaCelulares/src/Menu.java
@@ -7,11 +7,12 @@ public class Menu {
         Scanner entrada = new Scanner(System.in);
         System.out.println("Elija opcion");
         Integer opcion = entrada.nextInt();
-        entrada.close();
+
         switch (opcion){
             case 1:
                 MenuItemMostrar menu = new MenuItemMostrar(StockCel.getListaMotorola());
                 menu.mostrarLista();
+
         }
     }
 }
--- TiendaCelulares/src/StockCel.java
+++ TiendaCelulares/src/StockCel.java
@@ -5,8 +5,9 @@ public class StockCel {
 
     private static void llenarMotorola(){
         Celular cel1 = new Celular("Motorola", "Moto g200", "Snapdragon 880","108mp x 13mp", 90000);
-
+        Celular cel2 = new Celular("Motorola", "Moto g712", "Snapdragon 865", "50mp x 10", 65000);
         listaMotorola.add(cel1);
+        listaMotorola.add(cel2);
     }
 
     public static ArrayList<Celular> getListaMotorola() {
