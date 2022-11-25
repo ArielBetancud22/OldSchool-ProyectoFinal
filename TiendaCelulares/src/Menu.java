fa417869ffcd0b5d4570cca99d0c3ec261a57393
yair mathez <yair_mathez_9@hotmail.com>
2022-11-24 21:04:11 -0300

    Avance Celus

--- TiendaCelulares/src/Menu.java
+++ TiendaCelulares/src/Menu.java
@@ -4,15 +4,37 @@ public class Menu {
     public static void display(){
         System.out.println("1.Motorola");
         System.out.println("2.Apple");
+        System.out.println("3. LG");
+        System.out.println("4.Huawei");
+        System.out.println("5. Xiaomi");
         Scanner entrada = new Scanner(System.in);
         System.out.println("Elija opcion");
         Integer opcion = entrada.nextInt();
 
-        switch (opcion){
+        switch (opcion) {
             case 1:
-                MenuItemMostrar menu = new MenuItemMostrar(StockCel.getListaMotorola());
-                menu.mostrarLista();
+                MenuItemMostrar menuMotorola = new MenuItemMostrar(StockCel.getListaMotorola());
+                menuMotorola.mostrarLista();
+                break;
+            case 2:
+                MenuItemMostrar menuApple = new MenuItemMostrar(StockCel.getListaApple());
+                menuApple.mostrarLista();
+                break;
 
+            case 3:
+                MenuItemMostrar menuLg = new MenuItemMostrar(StockCel.getListaLg());
+                menuLg.mostrarLista();
+                break;
+
+            case 4:
+                MenuItemMostrar menuHuawei = new MenuItemMostrar(StockCel.getListaHuawei());
+                menuHuawei.mostrarLista();
+                break;
+
+            case 5: MenuItemMostrar menuXiaomi = new MenuItemMostrar(StockCel.getListaXiaomi());
+            menuXiaomi.mostrarLista();
+            break;
         }
     }
 }
+
