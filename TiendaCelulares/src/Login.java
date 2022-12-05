aac4e4fb88f7f3c3ef68c39c55644d26505db81b
panchotoni <panchotonidandel97@gmail.com>
2022-12-05 19:39:42 -0300

    Login

--- TiendaCelulares/src/Login.java
+++ TiendaCelulares/src/Login.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Login {
     public static boolean login() {
-        return true;
+        var usuario1 = "ariel";
+        var contraseña1 = 1234;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Dijite su usuario: ");
+        var usuario = Integer.parseInt(entrada.nextLine());
+        String usuarioS = Integer.toString(usuario) ;
+        System.out.println("Digite su contraseña: ");
+        var contraseña = Integer.parseInt(entrada.nextLine());
+
+        if(usuarioS == usuario1 || contraseña == contraseña1) {
+            return true;
+        }  else {
+            return false;
+        }
     }
 }
+
